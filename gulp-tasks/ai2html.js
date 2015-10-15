b9df3651785d37f74f50bc1e39646d6d440c2f49
Gabriel Florit <gabrielflorit@gmail.com>
2015-10-14 22:15:35 -0400

    INitial commit

--- gulp-tasks/ai2html.js
+++ gulp-tasks/ai2html.js
@@ -1,12 +1,12 @@
 var gulp    = require('gulp');
 var changed = require('gulp-changed');
 
-var src = 'src/html/partials/ai2html/**/*';
+var src = 'src/html/partials/ai2html/assets/**/*';
 
 gulp.task('ai2html-dev', function() {
 
 	return gulp.src(src)
-		.pipe(changed('dist/dev/assets'))
-		.pipe(gulp.dest('dist/dev/assets'));
+		.pipe(changed('src/assets'))
+		.pipe(gulp.dest('src/assets'));
 
 });
--- gulp-tasks/default.js
+++ gulp-tasks/default.js
@@ -3,15 +3,13 @@ var runSequence = require('run-sequence');
 
 // Default task to be run with `gulp`
 gulp.task('default', ['dev'], function () {
-	gulp.watch('src/css/*.styl',                 ['css-dev']);
-	gulp.watch('src/data/*.json',                ['html-dev']);
-	gulp.watch('src/html/**/*.hbs',              ['html-dev']);
-	gulp.watch('src/js/**/*.js',                 ['js-dev']);
-	
-	gulp.watch('src/html/partials/ai2html/**/*', ['ai2html-dev']);
-	
-	gulp.watch('src/assets/**/*',                ['assets-dev']);
-	gulp.watch('dist/dev/index.html',            ['browser-sync-reload']);
+	gulp.watch('src/css/*.styl',                        ['css-dev']);
+	gulp.watch('src/data/*.json',                       ['html-dev']);
+	gulp.watch('src/html/**/*.hbs',                     ['html-dev']);
+	gulp.watch('src/js/**/*.js',                        ['js-dev']);
+	gulp.watch('src/html/partials/ai2html/assets/**/*', ['ai2html-dev']);
+	gulp.watch('src/assets/**/*',                       ['assets-dev']);
+	gulp.watch('dist/dev/index.html',                   ['browser-sync-reload']);
 });
 
 gulp.task('dev', function() {
@@ -19,10 +17,8 @@ gulp.task('dev', function() {
 		'clean-dev',
 		'css-dev',
 		'js-dev',
-		'assets-dev',
-		
 		'ai2html-dev',
-		
+		'assets-dev',
 		'html-dev',
 		'browser-sync'
 	);
